94b949aacb733336c43279f66b19aafb6043edd8
porec <32462260+porec@users.noreply.github.com>
2021-11-25 10:20:49 +0100

    Update reposcan.sh

--- scripts/reposcan.sh
+++ scripts/reposcan.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./twistcli coderepo scan --address $PC_CONSOLE_URL -u $PCUSER -p $PCPASS --repository $REPO_ID ./
-scandetails=$(curl -k -u $PCUSER:$PCPASS -H 'Content-Type: application/json' "$PC_CONSOLE_URL/api/v1/coderepos-ci?name=$REPO_ID"|jq '.[-1]')
-scanresult=$(curl -k -u $PCUSER:$PCPASS -H 'Content-Type: application/json' "$PC_CONSOLE_URL/api/v1/coderepos-ci?name=$REPO_ID"|jq '.[-1].pass')
+./twistcli coderepo scan --address $PC_CONSOLE_URL -u $PCUSER -p $PCPASS --repository $GITHUB_REPOSITORY ./
+scandetails=$(curl -k -u $PCUSER:$PCPASS -H 'Content-Type: application/json' "$PC_CONSOLE_URL/api/v1/coderepos-ci?name=$GITHUB_REPOSITORY"|jq '.[-1]')
+scanresult=$(curl -k -u $PCUSER:$PCPASS -H 'Content-Type: application/json' "$PC_CONSOLE_URL/api/v1/coderepos-ci?name=$GITHUB_REPOSITORY"|jq '.[-1].pass')
 
 if [ "$scanresult" == $PRE_BUILD_GIT_SCAN_FLAG ]; then
    echo "Code Repo scan passed!"
